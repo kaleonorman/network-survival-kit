8f97dfe439a70aae9f24f3d7c3ac15467b3d77d1
Kaleo Norman <kaleonorman@gmail.com>
2018-01-02 20:47:55 -1000

    Update MAC.py
    Fixed variables

--- MAC.py
+++ MAC.py
@@ -1,7 +1,7 @@
 import pprint
 import requests
 MAC_URL = 'http://macvendors.co/api/%s'
-MAC = input('Please input MAC Address: ')
-r = requests.get(MAC_URL % MAC)
+mac = input('Please input MAC Address: ')
+r = requests.get(MAC_URL % mac)
 
 pprint.pprint(r.json())
