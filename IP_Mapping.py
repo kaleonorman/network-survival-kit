841ce0af6147a920e10cf15232710913594a5ebf
Kaleo Norman <kaleonorman@gmail.com>
2018-01-02 20:44:46 -1000

    Update IP_Mapping.py
    Fixed syntax

--- IP_Mapping.py
+++ IP_Mapping.py
@@ -3,6 +3,6 @@
 import socket
 
 name = socket.gethostname()
-ip = socket.gethostbyname(name);
+ip = socket.gethostbyname(name)
 
-print ("The IP address of target is: ", ip);
+print ("The IP address of target is: ", ip)
